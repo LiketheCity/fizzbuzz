dbb5ef38033076ff29e3347ce5b48f3f9d3f4fe8
LiketheCity <sydney.n.ruffin@gmail.com>
2020-12-09 22:57:24 -0600

    completed assignment
    Added comments, finished styling, condensed code.

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
+/* This for loop displays #s from 1-100. 
+Every # that is a multiple of 3 will display "Fizz" 
+Every # that is a multiple of 7 will display "Buzz"
+Every # that is a multiple of 3 & 7 will display "FizzBuzz" */
+
 for ( var i = 1; i <= 100; i++ )
 {
   if ( i%3 === 0 && i%7 === 0 )
   {
     document.write(i + " Fizzbuzz");
-    console.log( i + " FizzBuzz" );
   }
   else if ( i%3 === 0 ) 
   {
     document.write(i + " Fizz");
-    console.log(i+ " Fizz" );
   }
   else if ( i%7 === 0 ) 
   {
     document.write(i + " Buzz")
-    console.log(i+ " Buzz" );
   }
   else
   {
     document.write(i);
-    console.log(i);
   }
+
+  /* Makes each number display on a new line */
+  document.write('<br>')
 }
 
 
